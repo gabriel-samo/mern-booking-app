d9467abbb1960554b132e4ed26214abd647790b5
Gabriel_Samo <103880208+gabriel-samo@users.noreply.github.com>
2025-06-23 17:20:48 +0300

    login api route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
+import authRoutes from "./routes/auth";
 import userRoutes from "./routes/users";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 app.listen(7000, () => {
